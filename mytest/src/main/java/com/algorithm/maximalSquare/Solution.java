534b5874ade478235367998a453bce2c381fce8b
neverkah <240440826@qq.com>
2022-01-22 19:11:03 +0800

    leetcode 221

--- mytest/src/main/java/com/algorithm/maximalSquare/Solution.java
+++ mytest/src/main/java/com/algorithm/maximalSquare/Solution.java
@@ -3,32 +3,12 @@ package com.algorithm.maximalSquare;
 import java.util.ArrayList;
 import java.util.List;
 
-class Solution {
-    public static void main(String[] args) {
-
-        char[][] matrix = new char[][]{{'1', '0', '1', '0', '0'}, {'1', '0', '1', '1', '1'}, {'1', '1', '1', '1', '1'}, {'1', '0', '0', '1', '0'}};
-//        char[][] matrix = new char[][]{{'0','1'},{'1','0'}};
-//          char[][] matrix = new char[][]{{'0', '1'}};
-//        char[][] matrix = new char[][]{{'1', '0'}, {'1', '0'}};
-        System.out.println(new Solution().maximalSquare(matrix));
-//        System.out.println("hello");
-    }
-
+public class Solution {
     public int maximalSquare(char[][] matrix) {
-
         int result = 0;
-
-        //  matrix = new char[][]{{'1', '0', '1', '0', '0'}, {'1', '0', '1', '1', '1'}, {'1', '1', '1', '1', '1'}, {'1', '0', '0', '1', '0'}};
-
-//        System.out.println(matrix[0].length);
-
         List<Region>[][] linkedLists = new ArrayList[matrix.length][matrix[0].length];
-
-
         int continuousSize = 0;
         for (int i = 0; i < matrix.length; i++) {
-
-
             if (matrix[i][0] == '1') {
                 continuousSize++;
             } else {
@@ -39,17 +19,12 @@ class Solution {
                 Region region = new Region(continuousSize, 1);
                 regionResult.add(region);
                 if (region.isSqare()) {
-                    result = Math.max(result, region.area());
+                    result = Math.max(result, region.getHeight());
                 }
-
             }
-
             linkedLists[i][0] = regionResult;
-
-
         }
         for (int j = 1; j < matrix[0].length; j++) {
-
             continuousSize = 0;
             for (int i = 0; i < matrix.length; i++) {
                 if (matrix[i][j] == '1') {
@@ -57,80 +32,53 @@ class Solution {
                 } else {
                     continuousSize = 0;
                 }
-
-
                 int index = linkedLists[i][j - 1].size() + 1;
-
                 List<Region> regionList = new ArrayList<>();
                 if (continuousSize > 0) {
-
-
                     if (linkedLists[i][j - 1].size() > 0) {
-
-
                         for (int k = linkedLists[i][j - 1].size() - 1; k >= 0; k--) {
-
-
                             Region region = linkedLists[i][j - 1].get(k);
                             if (continuousSize <= region.getHeight()) {
                                 index--;
                             } else {
                                 break;
                             }
-
                         }
-
-
                     }
-
-
                     for (int k = 0; k < index; k++) {
                         Region region = null;
-                        if (k ==  linkedLists[i][j - 1].size()) {
+                        if (k == linkedLists[i][j - 1].size()) {
                             region = new Region(continuousSize, 1);
                             regionList.add(region);
                         } else {
                             region = new Region(Math.min(linkedLists[i][j - 1].get(k).getHeight(), continuousSize), linkedLists[i][j - 1].get(k).getLength() + 1);
-
-
                         }
-
                         if (region.getLength() > region.getHeight()) {
-
                         } else if (region.getLength() == region.getHeight()) {
-                            result = Math.max(result, region.area());
+                            result = Math.max(result, region.getHeight());
                         } else {
                             regionList.add(region);
                         }
-
                     }
-
-
                 }
-
                 linkedLists[i][j] = regionList;
-
-
             }
-
         }
-
-
-        return result;
+        return result * result;
     }
 }
 
 class Region {
-
     int height;
     int length;
 
-    public int area(){
-        return height*length;
-    }
     public Region(int height, int length) {
         this.height = height;
         this.length = length;
+    }
+
+    public int area() {
+        return height * length;
     }
 
     public boolean isSqare() {
