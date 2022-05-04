188cad1d48153486a82194d6fad9111aa02c4154
neverkah <240440826@qq.com>
2022-05-04 10:57:18 +0800

    leetcode

--- mytest/src/main/java/com/algorithm/minimumWindowSubstring/T.java
+++ mytest/src/main/java/com/algorithm/minimumWindowSubstring/T.java
@@ -1,20 +1,8 @@
 package com.algorithm.minimumWindowSubstring;
 
 public class T {
-
     public static void main(String[] args) {
-
         String s = "ADOBECODEBANC", t = "ABC";
-
         System.out.println(new Solution2().minWindow(s, t));
-
-        char c = '中';
-
-//        for (char i = Character.MIN_VALUE; true; i++) {
-//            System.out.println(i);
-//        }
-
-
     }
-
 }
