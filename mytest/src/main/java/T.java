27c6cfe0db24d18934f0ee9a1b16cf9f0c1e0be2
neverkah <240440826@qq.com>
2022-10-29 22:43:28 +0800

    111

--- mytest/src/main/java/T.java
+++ mytest/src/main/java/T.java
@@ -2,7 +2,7 @@ public class T {
 
     public static void main(String[] args) {
         String s1 = "eat";
-        String s2 = "tea";
+        String s2 = "tea2sfdsf322";
         System.out.println("dfgdgdfgdf");
         System.out.println(s1.hashCode() == s2.hashCode());
     }
