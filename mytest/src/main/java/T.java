3f898d35d83b09776a357b1beaa246844b0d43eb
towjeff <cblue2013@126.com>
2022-10-29 22:19:13 +0800

    Update T.java
    1111

--- mytest/src/main/java/T.java
+++ mytest/src/main/java/T.java
@@ -2,8 +2,8 @@ public class T {
 
     public static void main(String[] args) {
         String s1 = "eat";
-        String s2 = "tea";
-        System.out.println("xxxxxx");
+        System.out.println("xxxxxx111sdsdfds");
+        System.out.println("dfdsfdsfds");
         System.out.println(s1.hashCode() == s2.hashCode());
     }
 }
