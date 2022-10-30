249211993cefdf180472ca97b596935591ddea21
neverkah <240440826@qq.com>
2022-10-30 20:26:49 +0800

    ttttttt

--- mytest/src/main/java/T.java
+++ mytest/src/main/java/T.java
@@ -2,9 +2,22 @@ public class T {
 
     public static void main(String[] args) {
         String s1 = "eat";
-        String s2 = "tea2sfdsf322";
+
+        String s2 = "tea2sfdsf322111";
         System.out.println("dfgdgdfgdf");
-        System.out.println("aaaaaaa");
+        System.out.println("aaaaaaa2222");
+
+
+        System.out.println("dfgdgdfgdf");
+        System.out.println("sfdsfdsfds");
+
+        System.out.println("xxxxxx111sdsdfds");
+        System.out.println("dfdsfdsfds");
+//        String s2 = "tea";
+        System.out.println("xxxqwewewxxx");
+        System.out.println("dfgdgdfgdf");
+
+
         System.out.println(s1.hashCode() == s2.hashCode());
     }
 }
